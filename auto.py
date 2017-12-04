83595b2b5cb8622bced60bd00d9401b9574336f3
wanngwe <wanngwe@163.com>
2017-12-04 15:48:14 +0800

    增加12小时循环，完善1级建造师和二级建造师

--- auto.py
+++ auto.py
@@ -47,55 +47,64 @@ Label(frame1,text='发帖频率',fg = 'red',bg = 'white').grid(row=0,column=0,pa
 Entry(frame1,width=8,textvariable=v_fre,validate='key').grid(row=0,column=1,sticky=W)
 Label(frame1,text='秒').grid(row=0,column=2,sticky=W)
 Entry(frame1,width=8,textvariable=start,validate='key').grid(row=2,column=1,sticky=W)
-def calc(): 
-    from selenium import webdriver
-    from time import sleep
-    # 引入 Keys 模块
-    from selenium.webdriver.common.keys import Keys
-    from selenium.webdriver.support.select import Select
-    driver = webdriver.Chrome()
-    driver.get("http://passport.58guakao.com/User/login")
-    elem_user = driver.find_element_by_id("phone")
-    #elem_user.send_keys("13322991484")
-    elem_user.send_keys(PhoneNumber.get())
-    elem_pwd = driver.find_element_by_id("User_pwd")
-    elem_pwd.send_keys("ljx1234")
-    #elem_pwd.send_keys("zhiyi.6688")
-    elem_pwd.send_keys(Keys.RETURN)
-    sleep(2)
-    data=xlrd.open_workbook("58.xlsx")
-    table=data.sheets()[0]
-    nrows=table.nrows
-    print(nrows)
-	#sleep(2)
-    for index in range(start.get()-1,nrows):
-    	table.row_values(1)
-    	x=table.row_values(index)
-    	#pos=x[3].index('+')
-    	print(x[3])
-    	#x1=x[3][0:pos]
-    	#x2=x[3][pos+1:]
-    	driver.get("http://post.58guakao.com/index/subject?etype=5")
-    	driver.find_element_by_link_text(x[3]).click()
-    	sleep(1) 
-    	driver.find_element_by_link_text(x[4]).click()  	
-	
-               
-    	fa_item=driver.find_element_by_id("title");
-    	fa_item.send_keys(x[0]);
-    	s=Select(driver.find_element_by_id("province"))
-    	s.select_by_visible_text(x[2])
+def calc():
+    for i in range(12):
+        from selenium import webdriver
+        from time import sleep
+        # 引入 Keys 模块
+        from selenium.webdriver.common.keys import Keys
+        from selenium.webdriver.support.select import Select
+        driver = webdriver.Chrome()
+        driver.get("http://passport.58guakao.com/User/login")
+        elem_user = driver.find_element_by_id("phone")
+        #elem_user.send_keys("13322991484")
+        elem_user.send_keys(PhoneNumber.get())
+        elem_pwd = driver.find_element_by_id("User_pwd")
+        elem_pwd.send_keys("ljx1234")
+        #elem_pwd.send_keys("zhiyi.6688")
+        elem_pwd.send_keys(Keys.RETURN)
+        sleep(2)
+        data=xlrd.open_workbook("58.xlsx")
+        table=data.sheets()[0]
+        nrows=table.nrows
+        print(nrows)
+        #sleep(2)
+    
+        for index in range(start.get(),start.get()+3):
+            table.row_values(1)
+            x=table.row_values(index)
+            #pos=x[3].index('+')
+            print(x[3])
+            #x1=x[3][0:pos]
+            #x2=x[3][pos+1:]
+            driver.get("http://post.58guakao.com/index/subject?etype=5")
+            driver.find_element_by_link_text(x[3]).click()
+            
+            sleep(1)
+            print(x[4])
+            driver.find_element_by_link_text(x[4]).click()
+           
+            
+                   
+            fa_item=driver.find_element_by_id("title");
+            fa_item.send_keys(x[0]);
+            s=Select(driver.find_element_by_id("province"))
+            s.select_by_visible_text(x[2])
+            
+            driver.find_element_by_xpath('//input[@value="3"]').send_keys(Keys.SPACE)  # send space
+            sleep(1)
+            driver.find_element_by_xpath('//input[@value="3"]').click()  # click
+       
+            fa_content=driver.find_element_by_name("content");
+            fa_content.send_keys(x[1]);#内容随机
+            driver.find_element_by_id("publishsubmit").click()
+            sleep(v_fre.get())
+         
+        driver.quit()
+        print("i am sleep\r\n")
+        sleep(3600)
+        print("i am awake\r\n")
         
-    	driver.find_element_by_xpath('//input[@value="3"]').send_keys(Keys.SPACE)  # send space
-    	sleep(1)
-    	driver.find_element_by_xpath('//input[@value="3"]').click()  # click
-   
-    	fa_content=driver.find_element_by_name("content");
-    	fa_content.send_keys(x[1]);#内容随机
-    	driver.find_element_by_id("publishsubmit").click()
-    	sleep(v_fre.get())
-     
-    driver.quit()
 def stop_calc(): 
     stop=1
 Button(root,text='发帖',command=calc).grid(row=23,column=0,pady =40)
